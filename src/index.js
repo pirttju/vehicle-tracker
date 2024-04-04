b79dd119f4a440275cf1cf4658365f5702f5a1fe
Juhani Pirttilahti <juhani.pirttilahti@gmail.com>
2024-04-04 18:29:03 +0300

    Restructuring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 "use strict";
-require("dotenv").config();
-const Tile38 = require("tile38");
-
-// Ultra Fast Geospatial Database & Geofencing Server
-const tile38Client = new Tile38({
-  host: process.env.TILE38_HOST || "localhost",
-  port: process.env.TILE38_PORT || 9851,
-});
